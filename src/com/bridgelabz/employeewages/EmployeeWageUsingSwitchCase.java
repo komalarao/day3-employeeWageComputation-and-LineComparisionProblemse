e7e1553e1d2cc102d0f31e7454d4bd54168929d3
komalarao <manoharvakamullu1999@gmail.com>
2023-01-03 22:31:33 -0800

    Refactor: program for Caluculate DailyWage using switchCase

--- src/com/bridgelabz/employeewages/EmployeeWageUsingSwitchCase.java
+++ src/com/bridgelabz/employeewages/EmployeeWageUsingSwitchCase.java
@@ -1,21 +1,24 @@
 package com.bridgelabz.employeewages;
 import java.util.Random;
-public class AddingPartTimeEmployeeWage {
+public class EmployeeWageUsingSwitchCase {
 	
 public static void main(String[] args) {
 	  int wagePerHour = 20;
 	  int fullDayHours = 8;
 		Random attendance = new Random();
         int checkAttendance = attendance.nextInt(3);
-        if(checkAttendance == 1) {
+        switch (checkAttendance) {
+        case 1:
         	System.out.println("Employee Is Present:");
         	System.out.println("daily employee wage is:"+(wagePerHour*fullDayHours));
-        }else if (checkAttendance == 2){
+        	break;
+        case 2:
         	System.out.println("Employee Is Present But PartTime:");
         	System.out.println("part Time Employee daily wage is:"+((wagePerHour/2)*fullDayHours));
-
-        }else {
+        	break;
+        default:
         	System.out.println("Employee is Abscent:");
+        	break;
         }
 	}
 
