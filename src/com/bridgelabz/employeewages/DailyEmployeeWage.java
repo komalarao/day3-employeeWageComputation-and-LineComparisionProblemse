9d8e62382f89b583ea50c7ab63df6776a1576946
komalarao <manoharvakamullu1999@gmail.com>
2023-01-03 22:07:20 -0800

    Refactor: program for caluculate daily employee wage

--- src/com/bridgelabz/employeewages/DailyEmployeeWage.java
+++ src/com/bridgelabz/employeewages/DailyEmployeeWage.java
@@ -1,12 +1,15 @@
 package com.bridgelabz.employeewages;
 import java .util.Random;
-public class CheckEmployeeAttendance {
+public class DailyEmployeeWage {
 	
 public static void main(String[] args) {
+	  int wagePerHour = 20;
+	  int fullDayHours = 8;
 		Random attendance = new Random();
         int checkAttendance = attendance.nextInt(2);
         if(checkAttendance == 1) {
         	System.out.println("Employee Is Present:");
+        	System.out.println("daily employee wage is:"+(wagePerHour*fullDayHours));
         }else {
         	System.out.println("Employee Is Abscent:");
 
