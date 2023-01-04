e1ad0a618e219deb32674bec0f252a477dea67e3
komalarao <manoharvakamullu1999@gmail.com>
2023-01-04 01:01:21 -0800

    Refactor: program to caluculate wages till total workingHours or days are reached

--- src/com/bridgelabz/employeewages/EmployeeWageForMonthUsingSwitchCase.java
+++ src/com/bridgelabz/employeewages/EmployeeWageForMonthUsingSwitchCase.java
@@ -5,19 +5,37 @@ public class EmployeeWageForMonthUsingSwitchCase {
 public static void main(String[] args) {
 	  int wagePerHour = 20;
 	  int fullDayHours = 8;
-	  int totalWorkingDays = 20;
+	  int totalWorkingDays = 0;
+	  int totalWorkingHours=0;
 		Random attendance = new Random();
         int checkAttendance = attendance.nextInt(3);
+        Random checking = new Random();
+        int check = checking.nextInt(2);
         switch (checkAttendance) {
         case 1:
-        	System.out.println("Employee Is Present:");
-        	System.out.println("monthly employee wage is:"+(totalWorkingDays*wagePerHour*fullDayHours));
-        	System.out.println("daily employee wage is:"+(wagePerHour*fullDayHours));
+        	
+        	while(totalWorkingHours <=100 && totalWorkingDays<20) {
+        		if(check == 1) {
+        			totalWorkingDays++;
+        	
+        	System.out.println(" employee wage for "+totalWorkingDays+" days is: "+(totalWorkingDays*wagePerHour*fullDayHours));
+        		}else {
+                	System.out.println(" employee wage for "+totalWorkingHours+" hours is:"+(totalWorkingHours*wagePerHour));
+                     totalWorkingHours++;
+        		}
+        	}System.out.println("Employee Is Present and performing full time:");
         	break;
         case 2:
-        	System.out.println("Employee Is Present But PartTime:");
-        	System.out.println("part Time Employee monthly wage is:"+((wagePerHour/2)*fullDayHours*totalWorkingDays));
-        	System.out.println("part Time Employee daily wage is:"+((wagePerHour/2)*fullDayHours));
+
+        	while(totalWorkingHours<=100 && totalWorkingDays<20) {
+        		if(check ==1) {
+        			totalWorkingDays++;
+        	System.out.println("part Time Employee wage for "+totalWorkingDays+"days is: "+((wagePerHour/2)*fullDayHours*totalWorkingDays));
+        		}else {
+        			System.out.println("part Time Employee wage for "+totalWorkingHours+"hours is:"+((wagePerHour/2)*totalWorkingHours));
+        			totalWorkingHours++;
+        		}
+        	}System.out.println("Employee Is Present But PartTime:");
 
         	break;
         default:
