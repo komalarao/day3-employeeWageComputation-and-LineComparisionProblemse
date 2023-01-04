c185bedb35ac34ed54733045cae1f31865253baf
komalarao <manoharvakamullu1999@gmail.com>
2023-01-03 22:42:01 -0800

    Refactor: to caluculate monthly employee wage

--- src/com/bridgelabz/employeewages/EmployeeWageForMonthUsingSwitchCase.java
+++ src/com/bridgelabz/employeewages/EmployeeWageForMonthUsingSwitchCase.java
@@ -1,20 +1,24 @@
 package com.bridgelabz.employeewages;
 import java.util.Random;
-public class EmployeeWageUsingSwitchCase {
+public class EmployeeWageForMonthUsingSwitchCase {
 	
 public static void main(String[] args) {
 	  int wagePerHour = 20;
 	  int fullDayHours = 8;
+	  int totalWorkingDays = 20;
 		Random attendance = new Random();
         int checkAttendance = attendance.nextInt(3);
         switch (checkAttendance) {
         case 1:
         	System.out.println("Employee Is Present:");
+        	System.out.println("monthly employee wage is:"+(totalWorkingDays*wagePerHour*fullDayHours));
         	System.out.println("daily employee wage is:"+(wagePerHour*fullDayHours));
         	break;
         case 2:
         	System.out.println("Employee Is Present But PartTime:");
+        	System.out.println("part Time Employee monthly wage is:"+((wagePerHour/2)*fullDayHours*totalWorkingDays));
         	System.out.println("part Time Employee daily wage is:"+((wagePerHour/2)*fullDayHours));
+
         	break;
         default:
         	System.out.println("Employee is Abscent:");
