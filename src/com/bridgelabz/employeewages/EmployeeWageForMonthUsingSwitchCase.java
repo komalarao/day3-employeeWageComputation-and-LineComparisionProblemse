79120f7be7fab39bdcee4ec88842bc92fc8dc794
komalarao <116417072+komalarao@users.noreply.github.com>
2023-01-10 01:33:36 -0800

    Refactor: program for caluculate monthly wage by switch case

--- src/com/bridgelabz/employeewages/EmployeeWageForMonthUsingSwitchCase.java
+++ src/com/bridgelabz/employeewages/EmployeeWageForMonthUsingSwitchCase.java
@@ -3,21 +3,21 @@ import java.util.Random;
 public class EmployeeWageForMonthUsingSwitchCase {
 	
 public static void main(String[] args) {
-	  int wagePerHour = 20;
-	  int fullDayHours = 8;
-	  int totalWorkingDays = 20;
+	  int Wage_Per_Hour = 20;
+	  int Full_Day_Hours = 8;
+	  int Total_Working_Days = 20;
 		Random attendance = new Random();
-        int checkAttendance = attendance.nextInt(3);
-        switch (checkAttendance) {
+        int Check_Attendance = attendance.nextInt(3);
+        switch (Check_Attendance) {
         case 1:
         	System.out.println("Employee Is Present:");
-        	System.out.println("monthly employee wage is:"+(totalWorkingDays*wagePerHour*fullDayHours));
-        	System.out.println("daily employee wage is:"+(wagePerHour*fullDayHours));
+        	System.out.println("monthly employee wage is:"+(Total_Working_Days*Wage_Per_Hour*Full_Day_Hours));
+        	System.out.println("daily employee wage is:"+(Wage_Per_Hour*Full_Day_Hours));
         	break;
         case 2:
         	System.out.println("Employee Is Present But PartTime:");
-        	System.out.println("part Time Employee monthly wage is:"+((wagePerHour/2)*fullDayHours*totalWorkingDays));
-        	System.out.println("part Time Employee daily wage is:"+((wagePerHour/2)*fullDayHours));
+        	System.out.println("part Time Employee monthly wage is:"+((Wage_Per_Hour/2)*Full_Day_Hours*totalWorkingDays));
+        	System.out.println("part Time Employee daily wage is:"+((Wage_Per_Hour/2)*Full_Day_Hours));
 
         	break;
         default:
