b3373a900d6d7f1f01824f12a7820d836615a200
komalarao <116417072+komalarao@users.noreply.github.com>
2023-01-10 01:00:24 -0800

    Update DailyEmployeeWage.java

--- src/com/bridgelabz/employeewages/DailyEmployeeWage.java
+++ src/com/bridgelabz/employeewages/DailyEmployeeWage.java
@@ -3,13 +3,13 @@ import java .util.Random;
 public class DailyEmployeeWage {
 	
 public static void main(String[] args) {
-	  int wagePerHour = 20;
-	  int fullDayHours = 8;
+	  int WAGE_PERHOUR = 20;
+	  int FULL_DAYHOURS = 8;
 		Random attendance = new Random();
-        int checkAttendance = attendance.nextInt(2);
-        if(checkAttendance == 1) {
+        int Check_Attendance = attendance.nextInt(2);
+        if(Check_Attendance == 1) {
         	System.out.println("Employee Is Present:");
-        	System.out.println("daily employee wage is:"+(wagePerHour*fullDayHours));
+        	System.out.println("daily employee wage is:"+(WAGE_PERHOUR*FULL_DAYHOURS));
         }else {
         	System.out.println("Employee Is Abscent:");
 
