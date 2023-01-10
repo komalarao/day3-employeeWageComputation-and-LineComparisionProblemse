ef8168853e2398d346ee2e47272aa24fd96ea3d1
komalarao <116417072+komalarao@users.noreply.github.com>
2023-01-10 01:25:19 -0800

    Refactor: program for caluculate part time employee wage

--- src/com/bridgelabz/employeewages/AddingPartTimeEmployeeWage.java
+++ src/com/bridgelabz/employeewages/AddingPartTimeEmployeeWage.java
@@ -3,16 +3,16 @@ import java.util.Random;
 public class AddingPartTimeEmployeeWage {
 	
 public static void main(String[] args) {
-	  int wagePerHour = 20;
-	  int fullDayHours = 8;
+	  int Wage_Per_Hour = 20;
+	  int Full_Day_Hours = 8;
 		Random attendance = new Random();
-        int checkAttendance = attendance.nextInt(3);
-        if(checkAttendance == 1) {
+        int Check_Attendance = attendance.nextInt(3);
+        if(Check_Attendance == 1) {
         	System.out.println("Employee Is Present:");
-        	System.out.println("daily employee wage is:"+(wagePerHour*fullDayHours));
-        }else if (checkAttendance == 2){
+        	System.out.println("daily employee wage is:"+(Wage_Per_Hour*Full_Day_Hours));
+        }else if (Check_Attendance == 2){
         	System.out.println("Employee Is Present But PartTime:");
-        	System.out.println("part Time Employee daily wage is:"+((wagePerHour/2)*fullDayHours));
+        	System.out.println("part Time Employee daily wage is:"+((Wage_Per_Hour/2)*Full_Day_Hours));
 
         }else {
         	System.out.println("Employee is Abscent:");
