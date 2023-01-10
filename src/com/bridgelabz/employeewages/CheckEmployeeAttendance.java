a0ed7d8fdfc135441521e6452e8089954b6407bb
komalarao <116417072+komalarao@users.noreply.github.com>
2023-01-10 00:53:34 -0800

    Update CheckEmployeeAttendance.java

--- src/com/bridgelabz/employeewages/CheckEmployeeAttendance.java
+++ src/com/bridgelabz/employeewages/CheckEmployeeAttendance.java
@@ -4,8 +4,8 @@ public class CheckEmployeeAttendance {
 	
 public static void main(String[] args) {
 		Random attendance = new Random();
-        int checkAttendance = attendance.nextInt(2);
-        if(checkAttendance == 1) {
+        int Check_Attendance = attendance.nextInt(2);
+        if(Check_Attendance == 1) {
         	System.out.println("Employee Is Present:");
         }else {
         	System.out.println("Employee Is Abscent:");
