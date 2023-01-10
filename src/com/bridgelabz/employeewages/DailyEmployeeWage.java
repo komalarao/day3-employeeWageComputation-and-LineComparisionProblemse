f462d9db75a99049ce8be33fb92fc8ab11943605
komalarao <116417072+komalarao@users.noreply.github.com>
2023-01-10 01:19:22 -0800

    Refactor: employee wage program to caluculate daily wage

--- src/com/bridgelabz/employeewages/DailyEmployeeWage.java
+++ src/com/bridgelabz/employeewages/DailyEmployeeWage.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 	  int FULL_DAYHOURS = 8;
 		Random attendance = new Random();
         int Check_Attendance = attendance.nextInt(2);
+	
         if(Check_Attendance == 1) {
         	System.out.println("Employee Is Present:");
         	System.out.println("daily employee wage is:"+(WAGE_PERHOUR*FULL_DAYHOURS));
